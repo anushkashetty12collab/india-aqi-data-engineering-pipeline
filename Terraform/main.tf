1bdb2abd026bc124d2a3dfbe6da4827004d142b0
Anushka Shetty <anohanabrotherhoodcave@gmail.com>
2026-04-15 13:48:05 +0530

    Modify Google provider and add GCS and BigQuery resources
    Updated Google provider configuration and added resources for a GCS bucket and BigQuery dataset.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,13 +1,24 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "7.24.0"
-    }
-  }
+provider "google" {
+project = "project_id"
+region = "asia-south1"
 }
 
-provider "google" {
-  project     = "my-project-id"
-  region      = "us-central1"
+# -----------------------
+# GCS BUCKET
+# -----------------------
+resource "google_storage_bucket" "aqi_bucket" {
+name = "your_name_aqi-data-bucket"
+location = "ASIA-SOUTH1"
+force_destroy = true
+
+uniform_bucket_level_access = true
+}
+
+# -----------------------
+# BIGQUERY DATASET
+# -----------------------
+resource "google_bigquery_dataset" "aqi_dataset" {
+dataset_id = "aqi_dataset"
+location = "ASIA-SOUTH1"
+delete_contents_on_destroy = true
 }
